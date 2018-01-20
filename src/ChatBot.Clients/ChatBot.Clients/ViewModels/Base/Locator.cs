9ddb303a709a1b46a8d8a223bcd68c6d504d3932
Wilson Vargas <wilsonvargas_6@outlook.com>
2018-01-19 19:55:08 -0500

    Added Locator for MVVM pattern

---
+++ src/ChatBot.Clients/ChatBot.Clients/ViewModels/Base/Locator.cs
@@ -0,0 +1,63 @@
+﻿using Autofac;
+using ChatBot.Clients.Services.Dialog;
+using ChatBot.Clients.Services.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBot.Clients.ViewModels.Base
+{
+    public class Locator
+    {
+        private IContainer _container;
+        private ContainerBuilder _containerBuilder;
+
+        private static readonly Locator _instance = new Locator();
+
+        public static Locator Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public Locator()
+        {
+            _containerBuilder = new ContainerBuilder();
+            
+            _containerBuilder.RegisterType<DialogService>().As<IDialogService>();
+            _containerBuilder.RegisterType<NavigationService>().As<INavigationService>();
+          
+           
+           
+            _containerBuilder.RegisterType<LoginViewModel>();
+           
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+
+        public void Register<TInterface, TImplementation>() where TImplementation : TInterface
+        {
+            _containerBuilder.RegisterType<TImplementation>().As<TInterface>();
+        }
+
+        public void Register<T>() where T : class
+        {
+            _containerBuilder.RegisterType<T>();
+        }
+
+        public void Build()
+        {
+            _container = _containerBuilder.Build();
+        }
+    }
+}
---
+++ src/ChatBot.Clients/ChatBot.Clients/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,43 @@
+﻿using ChatBot.Clients.Services.Dialog;
+using ChatBot.Clients.Services.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChatBot.Clients.ViewModels.Base
+{
+    public class ViewModelBase : BindableObject
+    {
+        private bool _isBusy;
+
+        protected readonly IDialogService DialogService;
+        protected readonly INavigationService NavigationService;
+
+        public ViewModelBase()
+        {
+            DialogService = Locator.Instance.Resolve<IDialogService>();
+            NavigationService = Locator.Instance.Resolve<INavigationService>();
+        }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public virtual Task InitializeAsync(object navigationData)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
